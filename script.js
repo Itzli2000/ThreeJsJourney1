99d0fc33bf47eb55cde9d3fccb6e912608e2fcde
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2022-04-19 12:39:36 -0500

    Added threejs and created basic scene

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello threejs")
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+});
+const cube = new THREE.Mesh(geometry, material);
+cube.rotateX(0.5);
+cube.rotateY(0.5);
+scene.add(cube);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+camera.position.set(0, 0, 2);
+scene.add(camera);
+
+const canvas = document.querySelector("canvas.webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
